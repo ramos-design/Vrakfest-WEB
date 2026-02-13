eb41d44f1de180059a1c5abce1cb382a9a11af91
Artmovski <artmovski@gmail.com>
2026-02-13 13:06:56 +0100

    UI: Tiny mobile heading

--- components/LockScreen.tsx
+++ components/LockScreen.tsx
@@ -83,9 +83,9 @@ export const LockScreen = ({ onAuthenticate }: { onAuthenticate: () => void }) =
             alt="VRAKFEST Logo"
             className="h-20 md:h-28 w-auto mb-8 animate-fadeIn"
           />
-          <h1 className="text-sm md:text-6xl lg:text-7xl xl:text-8xl font-bebas font-bold leading-[1.2] md:leading-[0.9] mb-8 md:mb-12 animate-fadeIn uppercase tracking-tighter flex flex-col items-center gap-2 text-center px-4">
+          <h1 className="text-[10px] md:text-6xl lg:text-7xl xl:text-8xl font-bebas font-medium md:font-bold leading-[1.2] md:leading-[0.9] mb-8 md:mb-12 animate-fadeIn uppercase tracking-widest md:tracking-tighter flex flex-col items-center gap-2 text-center px-4">
             <span className="text-white">NOVÁ KAPITOLA PŘICHÁZÍ.</span>
-            <span className="text-[#F4CE14] drop-shadow-[0_0_20px_rgba(244,206,20,0.5)] md:drop-shadow-[0_0_50px_rgba(244,206,20,0.8)]">BUĎTE U TOHO S NÁMI!</span>
+            <span className="text-[#F4CE14] drop-shadow-[0_0_10px_rgba(244,206,20,0.3)] md:drop-shadow-[0_0_50px_rgba(244,206,20,0.8)]">BUĎTE U TOHO S NÁMI!</span>
           </h1>
 
           {/* Interactive Countdown / Login Input */}
